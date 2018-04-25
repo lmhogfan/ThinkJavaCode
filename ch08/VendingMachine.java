c1dbad956751a19f63c46ce8e6be9733a82c3792
Luke <lmhogfan@gmail.com>
2018-04-25 10:25:15 -0500

    VendingMachine progress

--- ch08/VendingMachine.java
+++ ch08/VendingMachine.java
@@ -11,7 +11,53 @@ public class VendingMachine
         printMenu(meals);
         System.out.println("Your selection: ");
         int[] numSold = new int[3];
-        printSales(numSold);
+        Scanner choose = new Scanner(System.in);
+        int sushi=0;
+        int spock=0;
+        int asparagus=0;
+        for (int x = 0; x >=0; x++)
+        {
+
+            int choice = choose.nextInt();
+            if (choice == 99)
+            {
+                break;
+            }
+            else if (choice == 0)
+                {
+                    numSold[0] = numSold[0] + 1;
+                    sushi = numSold[0];
+                    System.out.println("Thank you for choosing Alien Asparagus");
+                    System.out.println("Sold so far: ");
+                    System.out.println(sushi + " Freeze Dried Sushi");
+                    System.out.println(spock + " Spock's Brain Blast");
+                    System.out.println(asparagus + " Alien Asparagus");
+                }
+
+            else if (choice == 1)
+            {
+                numSold[1] = numSold[1] + 1;
+                spock=numSold[1];
+                System.out.println("Thank you for choosing Alien Asparagus");
+                System.out.println("Sold so far: ");
+                System.out.println(sushi+" Freeze Dried Sushi");
+                System.out.println(spock+" Spock's Brain Blast");
+                System.out.println(asparagus+" Alien Asparagus");
+            }
+
+            else if (choice == 2)
+            {
+                numSold[2] = numSold[2] + 1;
+                asparagus=numSold[2];
+                System.out.println("Thank you for choosing Alien Asparagus");
+                System.out.println("Sold so far: ");
+                System.out.println(sushi+" Freeze Dried Sushi");
+                System.out.println(spock+" Spock's Brain Blast");
+                System.out.println(asparagus+" Alien Asparagus");
+            }
+        }
+        System.out.println("Goodbye!");
+        printSales(sushi,spock,asparagus);
     }
 
     private static void printMenu(String[] menu)
@@ -23,42 +69,14 @@ public class VendingMachine
         }
     }
 
-    private static void printSales(int[] sales)
+    private static void printSales(int sushiPass, int spockPass, int asparagusPass)
     {
-        for (int totalSales : sales)
         {
-            Scanner choose = new Scanner(System.in);
-            int choice = choose.nextInt();
-            int sushi = 0;
-            int spock = 0;
-            int asparagus = 0;
-
-            if (choice == 99)
-                System.out.println("Goodbye!");
-            else if (choice == 0)
-            {
-                for (sushi = 0; sushi < 99; sushi++) ;
-                {
-                    sushi+=totalSales;
-                }
-            } else if (choice == 1)
-            {
-                for (totalSales = 0; totalSales < 99; totalSales++) ;
-                {
-                    spock = totalSales + 1;
-                }
-            } else if (choice == 2)
-            {
-                for (totalSales = 0; totalSales < 99; totalSales++) ;
-                {
-                    asparagus = totalSales + 1;
-                }
-            }
             System.out.println("Thank you for choosing Alien Asparagus");
-            System.out.println("Sold so far:");
-            System.out.println(sushi + " of Freeze Dried Sushi");
-            System.out.println(spock + " of Spock's Brain Blast");
-            System.out.println(asparagus + " of Alien Asparagus");
+            System.out.println("Sold so far: ");
+            System.out.println(sushiPass + " Freeze Dried Sushi");
+            System.out.println(spockPass + " Spock's Brain Blast");
+            System.out.println(asparagusPass + " Alien Asparagus");
         }
     }
 }
